308aaaf3a89807a29bc012da5f2a20622416ce3f
Deepak16061 <162699855+Deepak16061@users.noreply.github.com>
2025-01-18 23:08:57 +0530

    Add files via upload

---
+++ HTML,JS,CSS/day1.js
@@ -0,0 +1,27 @@
+2.75+4.99
+47.74
+(1+1)*3
+6
+0.1+0.2
+0.30000000000000004
+'hello'
+'hello'
+"hgello"
+'hgello'
+"some" + "strings"
+'somestrings'
+"some" + " " + "strings"
+'some strings'
+'some' +""+"strings"
+'somestrings'
+'$' + (20.95+7.99)
+'$28.939999999999998'
+'$' + Math.round(20.95+7.99)
+'$29'
+'hi i\'m deepak'
+"hi i'm deepak"
+`items(${1+1}): $${(2095+799)/100 }`
+'items(2): $28.94'
+`text
+some`
+'text\nsome'
